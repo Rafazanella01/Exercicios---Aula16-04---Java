bfb3982c050961817879754aa2ed27312f8c9f6c
Rafazanella01 <rafazanella2004@gmail.com>
2024-04-16 20:19:12 -0300

    Add projects

---
+++ Exercicio1/src/main/java/com/mycompany/exercicio1/main.java
@@ -0,0 +1,11 @@
+package com.mycompany.exercicio1;
+
+/**
+ *
+ * @author rafaz
+ */
+public class main {
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Exercicio2/src/main/java/com/mycompany/exercicio2/Exercicio2.java
@@ -0,0 +1,12 @@
+package com.mycompany.exercicio2;
+
+/**
+ *
+ * @author rafaz
+ */
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
