68fb9fd1872b0df8bcfeb0e14854296231699060
Rafazanella01 <rafazanella2004@gmail.com>
2024-04-18 23:41:25 -0300

    Update

--- ExercicioFuncionarios/src/main/java/com/mycompany/exerciciofuncionarios/MeuArrayList.java
+++ ExercicioFuncionarios/src/main/java/com/mycompany/exerciciofuncionarios/MeuArrayList.java
@@ -1,6 +1,5 @@
 package com.mycompany.exerciciofuncionarios;
 import com.mycompany.exerciciofuncionarios.Funcionario;
-import java.util.Iterator;
 import java.util.ArrayList;
 
 /**
--- ExercicioFuncionarios/src/main/java/com/mycompany/exerciciofuncionarios/exerciciofuncionarios.java
+++ ExercicioFuncionarios/src/main/java/com/mycompany/exerciciofuncionarios/exerciciofuncionarios.java
@@ -7,15 +7,15 @@ import com.mycompany.exerciciofuncionarios.MeuArrayList;
  *
  * @author rafaz
  */
-public class Main {
+public class exerciciofuncionarios {
      public static void main(String[] args) {
         System.out.println("Hello World!");
-        MeuArrayList ar = new MeuArrayList();
+        //MeuArrayList ar = new MeuArrayList();
          
-        Funcionario func = new Funcionario("Rafael Zanella", "Programador", 1300.32);
+        //Funcionario func = new Funcionario("Rafael Zanella", "Programador", 1300.32);
            
-        ar.adicionarElementoArrayList(func);
+        //ar.adicionarElementoArrayList(func);
        
-        ar.exibirElementosArrayList();
+        //ar.exibirElementosArrayList();
     }
 }
